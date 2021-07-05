a434579e324c91db7c8f5401f600f634700c1f1d
Eugen <undergate777@gmail.com>
2021-07-05 22:06:58 +0300

    Beginning

---
+++ Vector3D.cpp
@@ -0,0 +1,93 @@
+#include <cmath>
+
+struct Vector3D
+{
+	float x, y, z;
+
+	Vector3D() = default;
+
+	Vector3D(float a, float b, float c)
+	{
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	float& operator [] (int i)
+	{
+		return ((&x)[i]);
+	}
+
+	const float& operator [](int i) const
+	{
+		return ((&x)[i]);
+	}
+
+	Vector3D& operator +=(const Vector3D& v)
+	{
+		x += v.x;
+		y += v.y;
+		z += v.z;
+		return (*this);
+	}
+
+	Vector3D& operator -=(const Vector3D& v)
+	{
+		x -= v.x;
+		y -= v.y;
+		z -= v.z;
+		return (*this);
+	}
+
+	Vector3D& operator *=(float s)
+	{
+		x *= s;
+		y *= s;
+		z *= s;
+		return (*this);
+	}
+
+	Vector3D& operator /=(float s)
+	{
+		s = 1.0f / s;
+		x *= s;
+		y *= s;
+		z *= s;
+	}
+};
+
+inline Vector3D operator +(const Vector3D& a, const Vector3D& b)
+{
+	return (Vector3D(a.x + b.x, a.y + b.y, a.z + b.z));
+}
+
+inline Vector3D operator -(const Vector3D& a, const Vector3D& b)
+{
+	return (Vector3D(a.x - b.x, a.y - b.y, a.z - b.z));
+}
+
+inline Vector3D operator *(const Vector3D& v, float s)
+{
+	return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+inline Vector3D operator /(const Vector3D& v, float s)
+{
+	s = 1.0F / s;
+	return (Vector3D(v.x * s, v.y * s, v.z * s));
+}
+
+inline Vector3D operator -(const Vector3D& v)
+{
+	return (Vector3D(-v.x, -v.y, -v.z));
+}
+
+inline float Magnitude(const Vector3D& v)
+{
+	return (sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
+}
+
+inline Vector3D Normalize(const Vector3D& v)
+{
+	return (v / Magnitude(v));
+}
